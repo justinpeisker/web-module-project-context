20d6a9171fc2bb45aca14e945e16968592c26f5b
Justin Peisker <justinpeisker@gmail.com>
2021-12-09 12:54:34 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import ProductContext from './Contexts/ProductContext';
+import CartContext from './Contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,20 +14,27 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value= {cart}>
+					<Navigation  />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				{/* Routes */}
+				<ProductContext.Provider value ={{products, addItem}}>
+					<Route exact path="/">
+						<Products  />
+					</Route>
+				</ProductContext.Provider>
+
+
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
